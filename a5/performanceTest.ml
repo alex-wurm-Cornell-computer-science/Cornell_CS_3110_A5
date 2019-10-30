344500c20080899e7175ed25f5d973e20bc44c66
Alex Wurm <aew98@cornell.edu>
2019-10-29 23:37:00 -0400

    Added performance testing capabilities

--- a5/performanceTest.ml
+++ a5/performanceTest.ml
@@ -31,6 +31,8 @@ let time (f : unit -> unit) : float =
   ignore (f ());
   Unix.gettimeofday () -. t 
 
+(* Add your own code here. *)
+
 module IntKey
   : KeySig with type t = int
 =
@@ -44,9 +46,6 @@ struct
   let format fmt i =
     Format.fprintf fmt "\"%d\"" i
 end
-
-module L = DictionarySet.Make(IntKey)(ListDictionary.Make)
-module T = DictionarySet.Make(IntKey)(TreeDictionary.Make)
 
 (* Random lists *)
 let randomlist10k = rnd_list 10000
@@ -126,12 +125,102 @@ let sortedlist900k = rnd_sort_list 900000
 
 let sortedlist1M = rnd_sort_list 1000000
 
-(* module ListDictTest (L : Dictionary.Dictionary with 
-type Key.t = int and type Value.t = int) (T : TreeDictionary ) *)
+module ListDictTest = functor (LD : DictionarySet.Set with 
+type Elt.t = IntKey.t) -> struct
 
-(* let istdicttest lst dict =
+  module L = DictionarySet.Make(IntKey)(ListDictionary.Make)
+  type t = L.t
+
+   let rec insert_all l d =
+    match l with
+    | [] -> d
+    | h::t -> let d' = L.insert h d in 
+              insert_all t d'
+
+    let rec test4 l d =
+      match l with
+      | [] -> ()
+      | h::t -> ignore(L.member h d);
+                ignore(L.member h d);
+                ignore(L.member h d);
+                ignore(L.member h d);
+                ignore(test4 t d)
+
+    let time_tests l d =
+      let timed_run = fun () -> d |> insert_all l |> test4 l in
+      time timed_run
+
+    let run_data =
+      Printf.printf "n,List-Asc,List-Rnd\n";
+      Printf.printf "10000,%f,%f\n" (time_tests sortedlist10k L.empty)
+      (time_tests randomlist10k L.empty);
+      Printf.printf "20000,%f,%f\n" (time_tests sortedlist20k L.empty)
+      (time_tests randomlist20k L.empty);
+      Printf.printf "30000,%f,%f\n" (time_tests sortedlist30k L.empty)
+      (time_tests randomlist30k L.empty);
+      Printf.printf "40000,%f,%f\n" (time_tests sortedlist40k L.empty)
+      (time_tests randomlist40k L.empty);
+      Printf.printf "50000,%f,%f\n" (time_tests sortedlist50k L.empty)
+      (time_tests randomlist50k L.empty);
+      Printf.printf "60000,%f,%f\n" (time_tests sortedlist60k L.empty)
+      (time_tests randomlist60k L.empty);
+      Printf.printf "70000,%f,%f\n" (time_tests sortedlist70k L.empty)
+      (time_tests randomlist70k L.empty);
+      Printf.printf "80000,%f,%f\n" (time_tests sortedlist80k L.empty)
+      (time_tests randomlist80k L.empty);
+      Printf.printf "90000,%f,%f\n" (time_tests sortedlist90k L.empty)
+      (time_tests randomlist90k L.empty);
+      Printf.printf "100000,%f,%f\n" (time_tests sortedlist100k L.empty)
+      (time_tests randomlist100k L.empty);
+
+end
+
+module TreeDictTest = functor (TD : DictionarySet.Set with
+type Elt.t = IntKey.t) -> struct
+
+  module T =  DictionarySet.Make(IntKey)(TreeDictionary.Make)
+  type t = T.t
 
   let rec insert_all l d =
     match l with
     | [] -> d
-    | [h::t] -> insert_all t (d.insert h 0 d) *)
+    | h::t -> let d' = T.insert h d in 
+              insert_all t d'
+
+  let rec test4 l d =
+    match l with
+    | [] -> ()
+    | h::t -> ignore(T.member h d);
+              ignore(T.member h d);
+              ignore(T.member h d);
+              ignore(T.member h d);
+              ignore(test4 t d)
+
+  let time_tests l d =
+    let timed_run = fun () -> d |> insert_all l |> test4 l in
+    time timed_run
+
+  let run_data =
+      Printf.printf "n,List-Asc,List-Rnd\n";
+      Printf.printf "100000,%f,%f\n" (time_tests sortedlist100k T.empty)
+      (time_tests randomlist100k T.empty);
+      Printf.printf "200000,%f,%f\n" (time_tests sortedlist200k T.empty)
+      (time_tests randomlist200k T.empty);
+      Printf.printf "300000,%f,%f\n" (time_tests sortedlist300k T.empty)
+      (time_tests randomlist300k T.empty);
+      Printf.printf "400000,%f,%f\n" (time_tests sortedlist400k T.empty)
+      (time_tests randomlist400k T.empty);
+      Printf.printf "500000,%f,%f\n" (time_tests sortedlist500k T.empty)
+      (time_tests randomlist500k T.empty);
+      Printf.printf "600000,%f,%f\n" (time_tests sortedlist600k T.empty)
+      (time_tests randomlist600k T.empty);
+      Printf.printf "700000,%f,%f\n" (time_tests sortedlist700k T.empty)
+      (time_tests randomlist700k T.empty);
+      Printf.printf "800000,%f,%f\n" (time_tests sortedlist800k T.empty)
+      (time_tests randomlist800k T.empty);
+      Printf.printf "900000,%f,%f\n" (time_tests sortedlist900k T.empty)
+      (time_tests randomlist900k T.empty);
+      Printf.printf "1000000,%f,%f\n" (time_tests sortedlist1M T.empty)
+      (time_tests randomlist1M T.empty);
+
+end
